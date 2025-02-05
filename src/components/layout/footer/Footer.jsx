a0fef63c0297620dc9387837f1f85695d7381b1a
Alamdar Bashirov <alamdarzb-sp202@code.edu.az>
2025-02-05 21:53:12 +0400

    footer design is ready

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import style from './Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={style.footer}>
+      <div className={style.container}>
+        <div className={`${style.heading}`}>
+          <div className={style.headingName}>
+            <h4>Contact Us</h4>
+          </div>
+          <div>
+            <ul>
+              <li><a href="">Phone</a></li>
+              <li><a href="">Insta</a></li>
+              <li><a href="">Face</a></li>
+              <li><a href="">Adress</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
